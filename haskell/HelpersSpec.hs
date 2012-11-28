6a196f253ac48421085cfae4712f30e1ce929855
jonyamo <jon@jonyamo.net>
2012-11-28 12:07:05 -0500

    add more haskell helper specs

--- haskell/HelpersSpec.hs
+++ haskell/HelpersSpec.hs
@@ -6,5 +6,15 @@ import Helpers
 main :: IO ()
 main = hspec $ do
   describe "fibs" $ do
-    it "should return a list of fibonacci terms" $
+    it "should return a list of fibonacci numbers" $
       [x | x <- takeWhile (< 100) fibs] `shouldBe` [1,1,2,3,5,8,13,21,34,55,89]
+
+  describe "primes" $ do
+    it "should return a list of prime numbers" $
+      [x | x <- takeWhile (< 50) primes]
+      `shouldBe`
+      [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47]
+
+  describe "primeFactors" $ do
+    it "should return a list of prime factors for a given number" $
+      primeFactors 13195 `shouldBe` [5,7,13,29] 
